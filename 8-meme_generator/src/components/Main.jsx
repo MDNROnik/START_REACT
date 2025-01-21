c7b619e13bf7362e22510cfe807c77a35e94c3af
MDNROnik <nimurrahnananik@gmail.com>
2025-01-21 17:15:19 +0600

    Formate

--- 8-meme_generator/src/components/Main.jsx
+++ 8-meme_generator/src/components/Main.jsx
@@ -1,79 +1,71 @@
-import React, { useEffect } from "react"
+import React, { useEffect } from "react";
 export default function Main() {
-    const [count, setcount] = React.useState(0);
-    const [meme, setMeme] = React.useState({
-        topText: "One does not simply",
-        bottomText: "Walk into Mordor",
-        imgUrl: ""
-    });
+  const [count, setcount] = React.useState(0);
+  const [meme, setMeme] = React.useState({
+    topText: "One does not simply",
+    bottomText: "Walk into Mordor",
+    imgUrl: "",
+  });
 
-    function handlefuction(event){
-        console.log(event.currentTarget.name);
-        const{value, name} = event.currentTarget;
-        setMeme(
-            (prevMeme)=>(
-                {
-                    ...prevMeme,
-                    [name]:value
-                }
-            )
-        )
-    }
-    useEffect(
-        ()=>{
-            fetch("https://api.imgflip.com/get_memes")
-            .then( res => res.json() )
-            .then(obj=> {
-                const {data} = obj;
-                const {memes} = data;
-                console.log(data);
-                const randomNumber = Math.floor(Math.random() * memes.length)
-                const u = memes[randomNumber].url;
-                setMeme(
-                    (meme)=>(
-                        {
-                            ...meme,
-                            imgUrl:u
-                        }
-                    )
-                )
-            })
+  function handlefuction(event) {
+    console.log(event.currentTarget.name);
+    const { value, name } = event.currentTarget;
+    setMeme((prevMeme) => ({
+      ...prevMeme,
+      [name]: value,
+    }));
+  }
+  useEffect(() => {
+    fetch("https://api.imgflip.com/get_memes")
+      .then((res) => res.json())
+      .then((obj) => {
+        const { data } = obj;
+        const { memes } = data;
+        console.log(data);
+        const randomNumber = Math.floor(Math.random() * memes.length);
+        const u = memes[randomNumber].url;
+        setMeme((meme) => ({
+          ...meme,
+          imgUrl: u,
+        }));
+      });
+  }, [count]);
 
-        },
-        [count]
-    )
+  return (
+    <main>
+      <div className="form">
+        <label>
+          Top Text
+          <input
+            type="text"
+            placeholder="One does not simply"
+            name="topText"
+            onChange={handlefuction}
+          />
+        </label>
 
-    return (
-        <main>
-            <div className="form">
-                <label>Top Text
-                    <input
-                        type="text"
-                        placeholder="One does not simply"
-                        name="topText"
-                        onChange={handlefuction}
-                    />
-                </label>
-
-                <label>Bottom Text
-                    <input
-                        type="text"
-                        placeholder="Walk into Mordor"
-                        name="bottomText"
-                        onChange={handlefuction}
-                    />
-                </label>
-                <button onClick={
-                    function(){
-                        setcount(count+1);
-                    }
-                }>Get a new meme image 🖼</button>
-            </div>
-            <div className="meme">
-                <img src={meme.imgUrl} />
-                <span className="top">{meme.topText}</span>
-                <span className="bottom">{meme.bottomText}</span>
-            </div>
-        </main>
-    )
+        <label>
+          Bottom Text
+          <input
+            type="text"
+            placeholder="Walk into Mordor"
+            name="bottomText"
+            onChange={handlefuction}
+          />
+        </label>
+        <button
+          onClick={function () {
+            setcount(count + 1);
+          }}
+        >
+          Get a new meme image 🖼
+        </button>
+      </div>
+      <div className="meme">
+        <img src={meme.imgUrl} />
+        <span className="top">{meme.topText}</span>
+        <span className="bottom">{meme.bottomText}</span>
+      </div>
+    </main>
+  );
 }
