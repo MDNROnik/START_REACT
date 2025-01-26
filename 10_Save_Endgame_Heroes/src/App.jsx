40c50a04c0b2f27bd685f75254c5e0a509a446d4
MDNROnik <nimurrahnananik@gmail.com>
2025-01-26 19:52:35 +0600

    Design Level Finished

--- 10_Save_Endgame_Heroes/src/App.jsx
+++ 10_Save_Endgame_Heroes/src/App.jsx
@@ -62,7 +62,7 @@ function App() {
           onClick={() => {
             fun3(letter);
           }}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full m-1"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-sm m-1 "
         >
           {letter}
         </button>
@@ -74,7 +74,7 @@ function App() {
       return (
         <button
           key={key}
-          className="bg-blue-300 pointer-events-none hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+          className="bg-blue-300 pointer-events-none hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-sm m-1"
         >
           {letter}
         </button>
@@ -110,7 +110,7 @@ function App() {
       return (
         <span
           key={key}
-          class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+          className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
         >
           {hero}
         </span>
@@ -119,46 +119,69 @@ function App() {
   }
 
   return (
-    <>
-      <h1>{life}</h1>
-      <h1 className="bg-blue-500">{randomWord}</h1>
+    <div className="bg-neutral-950 h-screen p-10">
+      <div className=" ">
+        <h1>{life}</h1>
+        <h1 className="text-white">{randomWord}</h1>
 
-      <main>{fun4()}</main>
-      {foundWords === randomWord.length ? (
-        <h1>you win</h1>
-      ) : life <= 0 ? (
-        <h1>you lose</h1>
-      ) : (
-        <h1>keep playing</h1>
-      )}
-      {secretWord.map((letter, key) => {
-        if (letter.guessed) {
-          return (
-            <span
-              key={key}
-              className="inline-block bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-            >
-              {letter.letter}
-            </span>
-          );
-        } else {
-          return (
-            <span className="inline-block bg-gray-200 px-3 py-3 text-sm font-semibold text-gray-700 mr-2 mb-2">
-              {}
-            </span>
-          );
-        }
-      })}
-      <br />
-      <button onClick={fun}>new</button>
+        <header className="text-center  text-white p-4">
+          <h1 className="text-xl"> Save EndGame Heores</h1>
+          <p className="text-lg">
+            Guess the word within 10 attempts to keep the heroes from loss
+          </p>
+        </header>
 
-      <br />
-      {life > 0 && foundWords < randomWord.length ? (
-        <main>{fun2()}</main>
-      ) : (
-        <main>{fun5()}</main>
-      )}
-    </>
+        {foundWords === randomWord.length ? (
+          <section className="text-center bg-green-600 text-white flex flex-col items-center justify-center p-4 ">
+            <h2>You Win!</h2>
+            <p>Well Done!</p>
+          </section>
+        ) : life <= 0 ? (
+          <h1>you lose</h1>
+        ) : (
+          <section className="content-center bg-white flex flex-col items-center justify-center p-4 ">
+            <h2 className="text-xl">Keep Playing </h2>
+            <p>You Can Do IT</p>
+          </section>
+        )}
+        <main className="p-4">{fun4()}</main>
+        <br />
+        <section className="content-center border-b-green-500 flex items-center justify-center ">
+          {secretWord.map((letter, key) => {
+            if (letter.guessed) {
+              return (
+                <span
+                  key={key}
+                  className="inline-block bg-gray-200 px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                >
+                  {letter.letter}
+                </span>
+              );
+            } else {
+              return (
+                <span
+                  key={key}
+                  className="inline-block bg-gray-200 px-3 py-3 text-sm font-semibold text-black-700 mr-2 mb-2"
+                >
+                  {}
+                </span>
+              );
+            }
+          })}
+        </section>
+
+        <br />
+        <button onClick={fun}>new</button>
+
+        <br />
+
+        {life > 0 && foundWords < randomWord.length ? (
+          <section className="flex flex-wrap justify-center items-center">{fun2()}</section>
+        ) : (
+          <section className="flex flex-wrap justify-center items-center">{fun5()}</section>
+        )}
+      </div>
+    </div>
   );
 }
 
