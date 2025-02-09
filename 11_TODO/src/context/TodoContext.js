7d82dbb0a17dccaef4d80ab4d7b0763cf4fe8311
MDNROnik <nimurrahnananik@gmail.com>
2025-02-10 00:08:31 +0600

    context changes

--- 11_TODO/src/context/TodoContext.js
+++ 11_TODO/src/context/TodoContext.js
@@ -1,13 +1,6 @@
 import { createContext, useContext } from "react";
 
 export const TodoContext = createContext({
-  todos: [
-    {
-      id: 1,
-      todo: " TODO MESSAGE",
-      completed: false,
-    },
-  ],
   addTodo: (todo) => {},
   updateTodo: (id, todo) => {},
   deleteTodo: (id) => {},
