1a5ae23644f5ba555c72e5f3918e9fa5b123b46b
MDNROnik <nimurrahnananik@gmail.com>
2025-02-16 18:02:10 +0600

    develop blog proejct

---
+++ 12_Blog_Project/src/components/Container/container.jsx
@@ -0,0 +1,9 @@
+
+
+const container = ({Children}) => {
+  return (
+    <div className='w-full max-w-7xl mx-auto px-4' >{Children}</div>
+  )
+}
+
+export default container
--- 12_Blog_Project/src/components/index.js
+++ 12_Blog_Project/src/components/index.js
@@ -1,3 +1,6 @@
 import header from "./header/header";
 import footer from "./footer/footer";
-export { header, footer };
+import Container from './Container/container';
+import Logo from './Logo';
+import LogoutBtn from './header/LogoutBtn'
+export { header, footer, Container, Logo, LogoutBtn };
