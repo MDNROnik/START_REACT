207b8c110af96d28e0c79734c8bba5d5c89e682b
MDNROnik <nimurrahnananik@gmail.com>
2025-02-17 15:05:43 +0600

    Delete files

--- 12_Blog_Project/src/components/Container/container.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const container = ({Children}) => {
-  return (
-    <div className='w-full max-w-7xl mx-auto px-4' >{Children}</div>
-  )
-}
-
-export default container
--- 12_Blog_Project/src/components/index.js
+++
@@ -1,8 +0,0 @@
-import header from "./header/header";
-import footer from "./footer/footer";
-import Container from './Container/container';
-import Logo from './Logo';
-import LogoutBtn from './header/LogoutBtn'
-import Button from './Button'
-import Input from './Input'
-export { header, footer, Container, Logo, LogoutBtn, Button, Input };
